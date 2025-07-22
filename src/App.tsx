cd30fa9f56e1d91407c531a7b9f7c9c71263ca96
Dimqe <petr.makarov249@gmail.com>
2025-07-22 23:48:15 +0200

    hw3

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import './index.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import MovieGrid from './components/MovieGrid/MovieGrid';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import MovieModal from './components/MovieModal/MovieModal';
+import type{ Movie } from './types/movie';
+import { fetchMovies } from './services/movieService';
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    setIsLoading(true);
+    setError(false);
+    setMovies([]);
+
+    try {
+      const results = await fetchMovies(query);
+      if (results.length === 0) {
+        toast.error('No movies found for your request.');
+      }
+      setMovies(results);
+    } catch {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Toaster position="top-right" />
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      {error && <ErrorMessage />}
+      {movies.length > 0 && <MovieGrid movies={movies} onSelect={setSelectedMovie} />}
+      {selectedMovie && <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />}
+    </>
+  );
+}
